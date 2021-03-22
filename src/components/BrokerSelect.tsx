03138d9d04e864285bf1488b3918cea1b8a57d80
Andrii Miroshnychenko <andrew.miroshnichenko@gmail.com>
2021-03-22 15:18:20 +0100

    broker selector file added

---
+++ src/components/BrokerSelect.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { IBrokerSelectContainer } from "../containers/BrokerSelectContainer";
+
+type Props = IBrokerSelectContainer;
+
+export const BrokerSelect: React.FunctionComponent<Props> = () => {
+  return <div></div>;
+};
