480132028bff0452d7947f52a1a3048fa4883be0
Andrii Miroshnychenko <andrew.miroshnichenko@gmail.com>
2021-06-05 18:29:03 +0200

    files added

---
+++ src/pages/AboutRevolut/AboutRevolut.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AboutRevolut: React.FunctionComponent = () => (
+  <>
+    <Link to="/">Go Back</Link>
+    <div>Info page about Revolut</div>
+  </>
+);
+
+export default AboutRevolut;
